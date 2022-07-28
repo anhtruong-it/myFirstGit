1e7ff7725f9b9e02f9aaaa16797dddc99bd0daa4
www-data <www-data@s5168384.elf.ict.griffith.edu.au>
2022-07-28 22:26:07 +1000

    added adding function

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 console.log("Hello World");
+<<<<<<< HEAD
 console.log("Software Framework");
+=======
+function addingNumber(x, y){
+    sumNumber = x + y;
+    console.log(sumNumber);
+}
+addingNumber(5, 10);
+>>>>>>> added adding function
